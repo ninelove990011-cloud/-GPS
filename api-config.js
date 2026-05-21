6a748e412e08d6985103991aa0edf094ae78edf9
nine <ninelove990011@gmail.com>
2026-05-21 11:17:15 +0700

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx0gOL2FvCfMr-wXoU4F5lg_3HJSldJKcb35_T07pewdOE8UAVLVrKRDgGAkgc_7qZOaw/exec';
